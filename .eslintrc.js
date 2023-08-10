3576b1655288f9d128a5372ca0993d2848588fc9
Kanta Kurzumi <krzmknt@gmail.com>
2023-08-10 12:44:01 +0900

    initial commit
    - express with ts-node-dev template
    - cdk template

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("request received");
+  const result = {
+    statusCode: 200,
+    body: {
+      message: "Hello from Express and TypeScript!",
+    },
+  };
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
+});
